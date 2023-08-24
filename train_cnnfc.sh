fc5452efdf6268c931531710cc9d49922fa10214
bgzdaniel <bgz.daniel@gmail.com>
2023-08-24 12:51:01 +0200

    add training scripts

---
+++ train_cnnfc.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+#SBATCH -p dev
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32G
+#SBATCH --output=rl_output_cnnfc.txt
+
+module load anaconda/3
+source ~/.bashrc
+conda activate newest_torch
+
+python3 main.py play --no-gui --agents cnnfc_agent --train 1 --scenario coin-trainer --n-rounds 3000
+
--- train_fc.sh
+++ train_fc.sh
@@ -4,11 +4,11 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32G
-#SBATCH --output=daniel_rl_output.txt
+#SBATCH --output=rl_output_fc.txt
 
 module load anaconda/3
 source ~/.bashrc
 conda activate newest_torch
 
-python3 main.py play --no-gui --agents cnnfc_agent --train 1 --scenario coin-trainer --n-rounds 3000
+python3 main.py play --no-gui --agents simple_fc_agent peaceful_agent --train 1 --scenario coin-trainer --n-rounds 3000
 
