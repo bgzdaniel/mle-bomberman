3724fbadeb6b429950454bbdfbfe228fe864337d
001max <133685226+001max@users.noreply.github.com>
2023-10-02 16:10:48 +0200

    Update models.py

--- agent_code/PPO_agent/models.py
+++ agent_code/PPO_agent/models.py
@@ -13,7 +13,7 @@ ACTIONS_NUM = [0, 1, 2, 3, 4, 5]
 
 class ActorModel(nn.Module):
     def __init__(self, n_actions, input_dims, alpha,
-            Layer1_dims=17*20, Layer2_dims=119*20, save_dir=None):
+            Layer1_dims=17*20, Layer2_dims=119*20, save_dir=os.getcwd()):
         super(ActorModel, self).__init__()
 
         self.save_file = os.path.join(save_dir, 'PPO-actor')
@@ -25,11 +25,7 @@ class ActorModel(nn.Module):
             nn.Linear(Layer2_dims, n_actions),
             nn.Softmax(dim=-1))
 
-        if save_dir is None:
-            # Use the current working directory to save the model
-            self.save_dir = os.getcwd()
-        else:
-            self.save_dir = save_dir
+
 
         self.optimizer = optim.Adam(self.parameters(), lr=alpha)
         self.to(self.device)
@@ -49,7 +45,7 @@ class ActorModel(nn.Module):
 
 class CriticModel(nn.Module):
     def __init__(self, input_dims, alpha, Layer1_dims=17*20, Layer2_dims=119*20,
-            save_dir= None):
+            save_dir= os.getcwd()):
         super(CriticModel, self).__init__()
 
         self.save_file = os.path.join(save_dir, 'PPO-critic')
@@ -60,12 +56,6 @@ class CriticModel(nn.Module):
             nn.ReLU(),
             nn.Linear(Layer2_dims, 1))
 
-
-        if save_dir is None:
-            # Use the current working directory to save the model
-            self.save_dir = os.getcwd()
-        else:
-            self.save_dir = save_dir
             
         self.optimizer = optim.Adam(self.parameters(), lr=alpha)
         self.to(self.device)
